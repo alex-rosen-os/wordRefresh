779a97182d772a2f102d91ce0d60f799d4448ee8
Alex R <pencilandjelly@gmail.com>
2023-01-21 10:55:38 +0200

    removing unnecessary files

--- js/createUsers.js
+++ js/createUsers.js
@@ -1,26 +1,42 @@
-const fakeButton = document.querySelector(".fakeBtn");
-const container = document.querySelector(".users");
-const { fakeData } = require("./fakeData");
+// const fakeButton = document.querySelector(".fakeBtn");
+// const container = document.querySelector(".users");
 
-console.log(fakeData);
-const creatUser = async (e) => {
-  e.preventDefault()
 
-  const user = {
-    firstName: fakeData.firstName.value,
-    lastName: fakeData.lastName.value,
-    email: fakeData.email.value,
-  };
+const getUser = async () => {
+  // const users = {
+  //   firstName: fakeData.firstName.value,
+  //   lastName: fakeData.lastName.value,
+  //   email: fakeData.email.value,
+  // };
 
-  const response = await fetch("http://localhost:3000/users", {
-  method: "GET",
-  headers:{
-   'Content-Type': 'application/json'
-  },
-  body: JSON.stringify(user)
+  // const response = await fetch("http://localhost:3000/users", {
+  //   method: "GET",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: JSON.stringify(users),
+  // });
+  // return response;
 
-})
-return response;
+  const uri = "http://localhost:3000/users";
+  const res = await fetch(uri);
+  const users = await res.json();
+  console.log(users);
 };
 
-fakeButton.addEventListener("click", creatUser);
+window.addEventListener('DOMContentLoaded',()=> getUser())
+
+// function printUser(e) {
+
+//   e.preventDefault();
+//   const getU = getUser;
+
+//   const template = '';
+//   template+= `
+//   <div>${getU.email}</div>`
+
+//   return users.innerHTML = template;
+
+// }
+
+// fakeButton.addEventListener("click", getUser );
