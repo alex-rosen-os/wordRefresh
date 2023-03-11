8e4b58dd479c7cb05e4f0e534f9175775ea28157
Alex R <pencilandjelly@gmail.com>
2023-03-11 21:37:44 +0200

    changes

--- Actions/createUsers.js
+++ Actions/createUsers.js
@@ -1,23 +1,69 @@
-const fakeButton = document.querySelector(".fakeBtn");
-const container = document.querySelector(".users");
+const adverbsBtn = document.querySelector(".adverbs");
+const verbsBtn = document.querySelector(".verbs");
+const nounsBtn = document.querySelector(".nouns");
+const printVerbs = document.querySelector(".printVerbs");
+const printAdverbs = document.querySelector(".printAdverbs");
+const printNouns = document.querySelector(".printNouns");
 
-const getUser = async () => {
-  const uri = "http://localhost:3000/users";
+const getAdverbs = async () => {
+  const uri = "http://localhost:3000/words?=adverbData";
   const res = await fetch(uri);
-  const users = await res.json();
-  console.log(users);
+  const adverbs = await res.json();
+  console.log(adverbs);
 
   let template = "";
 
-  users.forEach((user) => {
+  adverbs.forEach((adverb) => {
     template += `
     <div>
-    ${user.email}
+    ${adverb.adverbData}
     </div>
+    
 
     `;
   });
-  container.innerHTML = template;
+  printAdverbs.innerHTML = template; 
 };
 
-fakeButton.addEventListener("click", getUser);
+adverbsBtn.addEventListener("click", getAdverbs);
+
+const getVerbs = async () => {
+  const uri = "http://localhost:3000/words";
+  const res = await fetch(uri);
+  const verbs = await res.json();
+  console.log(verbs);
+
+  let template = "";
+
+  verbs.forEach((verb) => {
+    template += `
+    <div>
+    ${verb.verbData}
+    </div>
+    
+
+    `;
+  });
+  printVerbs.innerHTML = template;
+};
+
+verbsBtn.addEventListener("click", getVerbs);
+
+const getNouns = async () => {
+  const uri = "http://localhost:3000/words?=nounData";
+  const res = await fetch(uri);
+  const nouns = await res.json();
+
+  let template = "";
+
+  nouns.forEach((noun) => {
+    template += `
+    <div>
+    ${noun.nounData}
+    </div>
+    `;
+  });
+  printNouns.innerHTML = template;
+};
+
+nounsBtn.addEventListener("click", getNouns);
--- Data/fakeData.js
+++ Data/fakeData.js
@@ -1,27 +1,42 @@
-// import {faker} from '@faker-js/faker'
 const { faker } = require("@faker-js/faker");
 
- function /* Generating 50 users with random data. */
- generateUsers() {
-  var users = [];
 
-  for (var id = 0; id < 50; id++) {
-    var firstName = faker.name.firstName();
-    var lastName = faker.name.lastName();
-    var email = faker.internet.email();
-    users.push({
+function generateWords() {
+  var words = [];
+
+  for (var id = 0; id < 5; id++) {
+    var verbData = faker.word.verb();
+    var adverbData = faker.word.adverb();
+    var nounData = faker.word.noun();
+    words.push({
       id: id,
-      first_name: firstName,
-      last_name: lastName,
-      email: email,
+      verbData: verbData,
+      adverbData: adverbData,
+      nounData: nounData
+
     });
   }
 
- return { users }; 
+  return { words };
 }
 
-/* Exporting the function `generateUsers` so that it can be used in other files. */
-module.exports = generateUsers;
+module.exports = generateWords;
 
+
+// function generateAdverbs() {
+//   var adverbs = [];
+
+//   for (var id = 0; id < 5; id++) {
+//     var adverbData = faker.word.adverb();
+//     adverbs.push({
+//       id: id,
+//       adverbData: adverbData
+//     });
+//   }
+
+//   return { adverbs };
+// }
+
+// module.exports = generateAdverbs;
 
 
